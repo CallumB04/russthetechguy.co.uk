ffd1175e7e87007f521e1daf08afd8b8cb50d5c6
Cal <callumburgoyne04@gmail.com>
2024-10-15 01:49:16 +0100

    feat: add device responsive dropdown menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
 /* Common functions used across all pages */
+
+const navbarDropdownButton = document.getElementById("navbar-dropdown-button");
+const dropdownMenu = document.getElementById("dropdown-menu");
+
+navbarDropdownButton.addEventListener("click", () => {
+
+    if (dropdownMenu.classList.contains("hidden")) {
+        dropdownMenu.classList.remove("hidden");
+
+        // Landscape
+        if (screen.width > screen.height) {
+            dropdownMenu.style.animation = "0.1s dropdown-appear linear";
+        } 
+        // Portait
+        else {
+            dropdownMenu.style.animation = "0.2s mobile-dropdown-appear linear";
+        }
+    } else {
+        // Landscape
+        if (screen.width > screen.height) {
+            setTimeout(() => {dropdownMenu.classList.add("hidden")}, 90);
+            dropdownMenu.style.animation = "0.1s dropdown-disappear linear";
+        }
+        // Portait
+        else {
+            setTimeout(() => {dropdownMenu.classList.add("hidden")}, 190);
+            dropdownMenu.style.animation = "0.2s mobile-dropdown-disappear linear";
+        }
+    }
+})
