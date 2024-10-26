d33f1ff473ec4817201e6a1a1ac4dae7d0eb0e54
Cal <callumburgoyne04@gmail.com>
2024-10-26 23:37:21 +0100

    chore: add image paths to array

--- scripts/pages/gallery.js
+++ scripts/pages/gallery.js
@@ -1,6 +1,11 @@
 /* Functions specifically for the gallery page (gallery.html) */
 
-const image_files = []; // list of file names in the /gallery directory
+const image_files = [
+    "phone1-before.jpg", "phone1-after.jpg",
+      "laptop1-before.jpg", "laptop1-after.jpg",
+        "workshop.jpg",
+         "phonerepair1.jpg"
+        ]; // list of file names in the /gallery directory
 const gallery = document.getElementById("gallery-section");
 
 // function to load the html for the images
