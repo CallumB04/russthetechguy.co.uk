94a42841d876331be572d3603e1c46fa15fee292
Cal <callumburgoyne04@gmail.com>
2024-10-26 18:17:59 +0100

    feat: adding image elements through javascript

--- scripts/pages/gallery.js
+++ scripts/pages/gallery.js
@@ -1,1 +1,13 @@
 /* Functions specifically for the gallery page (gallery.html) */
+
+const image_files = []; // list of file names in the /gallery directory
+const gallery = document.getElementById("gallery-section");
+
+// function to load the html for the images
+const load_images = () => {
+    for (const filename of image_files) {
+        gallery.innerHTML += `
+            <img src="../assets/images/gallery/${filename}" class="gallery-image"/>
+        `
+    }
+}
