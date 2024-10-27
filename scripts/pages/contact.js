9e6d3b1c162af561282fda24520a85f716c3b68c
Cal <callumburgoyne04@gmail.com>
2024-10-27 20:25:22 +0000

    feat: ensure user fills required input fields

--- scripts/pages/contact.js
+++ scripts/pages/contact.js
@@ -7,3 +7,25 @@ const reasoningInput = document.getElementById("reasoning-input");
 const messageInput = document.getElementById("message-input");
 
 const sendBtn = document.getElementById("send-button");
+
+sendBtn.addEventListener("click", 
+    () => {
+        
+        // Array for all inputs that need an entered value
+        let requiredInputValues = [
+            firstNameInput.value, 
+            lastNameInput.value, 
+            reasoningInput.value, 
+            messageInput.value
+        ];
+
+        // ensuring no required inputs are empty
+        if (requiredInputValues.every(value => value !== "")) {
+
+        } 
+        
+        // if required input is empty, alert user
+        else {
+            alert("Please fill in all required input fields!");
+        }
+});
