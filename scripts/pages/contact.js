ece59e63d209961b14569e18dbf42dd4c1b5f36a
Cal <callumburgoyne04@gmail.com>
2024-10-27 20:11:05 +0000

    chore: get all elements from id's

--- scripts/pages/contact.js
+++ scripts/pages/contact.js
@@ -1,1 +1,9 @@
 /* Functions specifically for the contact page (contact.html) */
+
+const firstNameInput = document.getElementById("fname-input");
+const lastNameInput = document.getElementById("lname-input");
+const phoneNumberInput = document.getElementById("phone-input");
+const reasoningInput = document.getElementById("reasoning-input");
+const messageInput = document.getElementById("message-input");
+
+const sendBtn = document.getElementById("send-button");
